b43b4fbc3d0af5d6390914711dd8a4d4fd2d3411
iamabdulrafay <upwork8000@gmail.com>
2024-01-27 18:58:32 +0500

    aaaaaaasd

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/react-assignment-portfolioWeb/", // Set the correct base URL
